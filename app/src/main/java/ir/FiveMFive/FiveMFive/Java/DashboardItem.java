9066398dc2d1bb711b2a9386996396f99b1a50e0
alireza <alirezaeshahi18@mail.com>
2024-05-28 12:16:12 +0330

    create DashboardItem.java

---
+++ app/src/main/java/ir/FiveMFive/FiveMFive/Java/DashboardItem.java
@@ -0,0 +1,22 @@
+package ir.FiveMFive.FiveMFive.Java;
+
+public class DashboardItem {
+    private int imageResID;
+    private String title;
+
+    public int getImageResID() {
+        return imageResID;
+    }
+
+    public void setImageResID(int imageResID) {
+        this.imageResID = imageResID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
