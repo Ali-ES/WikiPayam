b2b79d7ddec7ed922c556d1251072b3997d42e59
alireza <alirezaeshahi18@mail.com>
2024-05-29 11:14:05 +0330

    Implement OnClick for dashboard items

--- app/src/main/java/ir/FiveMFive/FiveMFive/ViewHolders/DashboardHolder.java
+++ app/src/main/java/ir/FiveMFive/FiveMFive/ViewHolders/DashboardHolder.java
@@ -2,25 +2,31 @@ package ir.FiveMFive.FiveMFive.ViewHolders;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import ir.FiveMFive.FiveMFive.Fragment.SendMessage.SendMessageFragment;
 import ir.FiveMFive.FiveMFive.Java.DashboardItem;
 import ir.FiveMFive.FiveMFive.R;
 
-public class DashboardViewHolder extends RecyclerView.ViewHolder {
+public class DashboardHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private DashboardItem dashboardItem;
     private ImageView icon;
     private TextView title;
-    public DashboardViewHolder(LayoutInflater inflater, ViewGroup parent) {
+    private Fragment fragment;
+    public DashboardHolder(LayoutInflater inflater, ViewGroup parent, Fragment fragment) {
         super(inflater.inflate(R.layout.card_dashboard_item, parent, false));
 
         icon = itemView.findViewById(R.id.icon);
         title = itemView.findViewById(R.id.title);
+        itemView.setOnClickListener(this);
     }
     public void bind(DashboardItem item, Context c) {
         dashboardItem = item;
@@ -30,5 +36,14 @@ public class DashboardViewHolder extends RecyclerView.ViewHolder {
 
         String titleText = item.getTitle();
         title.setText(titleText);
+    }
+    @Override
+    public void onClick(View view) {
+        Fragment f = new SendMessageFragment();
+        FragmentTransaction ft = fragment.getParentFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container, f);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
