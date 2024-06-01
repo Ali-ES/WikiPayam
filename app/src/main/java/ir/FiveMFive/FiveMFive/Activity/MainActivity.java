791391dba1c6dfb370e884b5c2d0df3a4aee00f2
alireza <alirezaeshahi18@mail.com>
2024-06-01 10:37:11 +0330

    move MainActivity action bar to DashboardFragment, bc action bar is not fixed for all fragments!

--- app/src/main/java/ir/FiveMFive/FiveMFive/Activity/MainActivity.java
+++ app/src/main/java/ir/FiveMFive/FiveMFive/Activity/MainActivity.java
@@ -2,17 +2,13 @@ package ir.FiveMFive.FiveMFive.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import ir.FiveMFive.FiveMFive.Fragment.DashboardFragment;
 import ir.FiveMFive.FiveMFive.R;
-import ir.FiveMFive.FiveMFive.Utility.PopupBuilder;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -27,18 +23,6 @@ public class MainActivity extends AppCompatActivity {
         ft.replace(R.id.fragment_container, fragment);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         ft.commit();
-
-
-        ConstraintLayout actionBar = findViewById(R.id.action_bar);
-        ImageView more = findViewById(R.id.more_iv);
-        more.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupBuilder popupBuilder = new PopupBuilder(MainActivity.this);
-                popupBuilder.addItem(R.drawable.ic_exit, R.string.exit);
-                popupBuilder.showPopup(actionBar);
-            }
-        });
 
 
     }
