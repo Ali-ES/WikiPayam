569d4641aac72d2456b1c0655b8c75b36b0952ec
alireza <alirezaeshahi18@mail.com>
2024-06-05 09:44:29 +0330

    Check login stat in MainActivity to start LoginActivity or continue to DashboardFragment

--- app/src/main/java/ir/FiveMFive/FiveMFive/Activity/MainActivity.java
+++ app/src/main/java/ir/FiveMFive/FiveMFive/Activity/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import ir.FiveMFive.FiveMFive.Fragment.DashboardFragment;
 import ir.FiveMFive.FiveMFive.R;
+import ir.FiveMFive.FiveMFive.Utility.CredentialCrypter;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -18,11 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        Fragment fragment = new DashboardFragment();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container, fragment);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
+        if(CredentialCrypter.hasLoggedIn(this)) {
+            Fragment fragment = new DashboardFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_container, fragment);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
 
 
 
