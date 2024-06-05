b1873b802f7404bb33481723c9c59edfd90bb3f4
alireza <alirezaeshahi18@mail.com>
2024-06-05 10:55:54 +0330

    implemented and test the biometric auth

--- app/src/main/java/ir/FiveMFive/FiveMFive/Activity/MainActivity.java
+++ app/src/main/java/ir/FiveMFive/FiveMFive/Activity/MainActivity.java
@@ -1,12 +1,20 @@
 package ir.FiveMFive.FiveMFive.Activity;
 
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.concurrent.Executor;
 
 import ir.FiveMFive.FiveMFive.Fragment.DashboardFragment;
 import ir.FiveMFive.FiveMFive.R;
@@ -21,11 +29,16 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         if(CredentialCrypter.hasLoggedIn(this)) {
-            Fragment fragment = new DashboardFragment();
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.fragment_container, fragment);
-            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-            ft.commit();
+            if(getIntent() != null) {
+                BiometricManager biometricManager = BiometricManager.from(this);
+                if (biometricManager.canAuthenticate(BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS) {
+                    biometricAuth();
+                } else {
+                    openDashboardFragment();
+                }
+            } else {
+                openDashboardFragment();
+            }
         } else {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
@@ -37,6 +50,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void biometricAuth() {
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt prompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                // error handle for biometric auth
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                openDashboardFragment();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                //biometric auth failed
+            }
+        });
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle(getString(R.string.user_authentication))
+                .setSubtitle(getString(R.string.biometric_auth))
+                .setNegativeButtonText(getString(R.string.passcode_login))
+                .build();
+
+        prompt.authenticate(promptInfo);
+    }
+    private void openDashboardFragment() {
+        Fragment fragment = new DashboardFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container, fragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+    }
     public void onBackPressed() {
         super.onBackPressed();
         if(getSupportFragmentManager().getBackStackEntryCount() < 0) {
