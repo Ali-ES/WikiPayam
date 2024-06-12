0fce7dca03b90a872c75ac868a9076cc4c999dc9
alireza <alirezaeshahi18@mail.com>
2024-06-12 14:32:56 +0330

    add constants needed for characters

--- app/src/main/java/ir/FiveMFive/FiveMFive/Utility/Constants.java
+++ app/src/main/java/ir/FiveMFive/FiveMFive/Utility/Constants.java
@@ -2,4 +2,6 @@ package ir.FiveMFive.FiveMFive.Utility;
 
 public class Constants {
     public static final String PREF_CREDENTIALS = "credentialsSharedPreferences";
+    public static final int DEFAULT_MESSAGE_CHARS = 70;
+    public static final String DEFAULT_MESSAGE_END = "لغو۱۱";
 }
