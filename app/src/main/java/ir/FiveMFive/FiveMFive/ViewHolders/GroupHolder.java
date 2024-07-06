496bd2b0a7aedb4ea9a335bdd3d11daa13d20643
alireza <alirezaeshahi18@mail.com>
2024-07-06 10:50:20 +0330

    Set up GroupHolder

---
+++ app/src/main/java/ir/FiveMFive/FiveMFive/ViewHolders/GroupHolder.java
@@ -0,0 +1,47 @@
+package ir.FiveMFive.FiveMFive.ViewHolders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import ir.FiveMFive.FiveMFive.Interface.ListModifyListener;
+import ir.FiveMFive.FiveMFive.R;
+import ir.FiveMFive.FiveMFive.RecyclerViewAdapter;
+
+public class GroupHolder extends RecyclerView.ViewHolder {
+    private TextView group;
+    private ImageView delete;
+    private RecyclerViewAdapter adapter;
+    private ListModifyListener listModifyListener;
+    public GroupHolder(LayoutInflater inflater, ViewGroup parent, RecyclerViewAdapter adapter) {
+        super(inflater.inflate(R.layout.card_removable_item, parent, false));
+        this.adapter = adapter;
+
+        group = itemView.findViewById(R.id.item_tv);
+        delete = itemView.findViewById(R.id.delete_iv);
+
+        handleDelete();
+    }
+    private void handleDelete() {
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(listModifyListener != null) {
+                    listModifyListener.onListRemove(getAdapterPosition());
+                }
+                adapter.notifyItemRemoved(getAdapterPosition());
+            }
+        });
+    }
+    public void bind(String number) {
+        group.setText(number);
+    }
+
+    public void setListModifyListener(ListModifyListener listener) {
+        this.listModifyListener = listener;
+    }
+}
