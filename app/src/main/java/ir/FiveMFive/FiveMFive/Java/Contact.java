254dcb95b21aee8fe43dfbc6717aaa24b781bd73
alireza <alirezaeshahi18@mail.com>
2024-07-13 12:32:57 +0330

    create Contact.java model object

---
+++ app/src/main/java/ir/FiveMFive/FiveMFive/Java/Contact.java
@@ -0,0 +1,31 @@
+package ir.FiveMFive.FiveMFive.Java;
+
+public class Contact {
+    private String name;
+    private String mobile;
+    private boolean isSelected;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+}
